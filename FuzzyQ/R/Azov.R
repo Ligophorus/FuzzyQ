cdc78a4e0cc91ab5a054dd438ab776334d328cb5
Juan Antonio Balbuena <balbuena.ja@gmail.com>
2020-09-14 14:48:07 +0200

    doc Azov

---
+++ FuzzyQ/R/Azov.R
@@ -0,0 +1,23 @@
+#' Helminth communities of so-iuy mullets from the Sea of Azov
+#'
+#' A dataset containing the abundance of 25 helminth species from 378 so-iuy
+#' mullets collected in the Sea of Azov and the Black Sea. Fish are grouped
+#' in seven surveys
+#'
+#'#' @docType data
+#'
+#' @usage data(grav)
+#'
+#' @format A data frame with 378 rows and 26 variables:
+#' \describe{
+#'   \item{sample}{survey identifier}
+#'   \item{Ssp1}{abundance (no. individuals) of Solostamenides sp. 1}
+#'   ...
+#' }
+#' @references Llopis‐Belenguer, C., Blasco‐Costa, I., Balbuena, J.A.,
+#' Sarabeev, V. and Stouffer, D.B. (2020), Native and invasive hosts play
+#' different roles in host–parasite networks. Ecography, 43: 559-568.
+#' doi:10.1111/ecog.04963
+#'
+#' @source \url{https://doi.org/10.7910/DVN/IWIKOL}
+"Azov"
